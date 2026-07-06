75234a01d57694a79ceb0be6c6146ff6811784d0
xbcyl813 <59333099+xbcyl813@users.noreply.github.com>
2026-07-06 14:44:28 +0800

    Update libkernel_sys_ext.c

--- src/libkernel_sys_ext.c
+++ src/libkernel_sys_ext.c
@@ -1,4 +1,11 @@
 asm(".global \"C49jelxiaVE\"\n"
     ".type \"C49jelxiaVE\" @function\n"
     "\"C49jelxiaVE\":\n");
+// ============================================================================
+// 【全新加入】：手动导出跨进程动态库注入函数存根（彻底解决 ld.lld 未定义符号链接报错）
+// ============================================================================
+asm(".global sceKernelLoadStartModuleForPid\n"
+    ".type sceKernelLoadStartModuleForPid @function\n"
+    "sceKernelLoadStartModuleForPid:\n"
+    "    jmp [rip + sceKernelLoadStartModuleForPid@GOTPCREL]\n");
 
